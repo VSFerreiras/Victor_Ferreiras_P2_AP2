37dd5ddd54f05a3522a85b511cc99e75afb6e362
victor <vsferreiras402@protonmail.com>
2025-10-29 19:13:29 -0400

    Configurado Entrypoint en MainActivity.kt

--- app/src/main/java/ucne/edu/victor_ferreiras_p2_ap2/MainActivity.kt
+++ app/src/main/java/ucne/edu/victor_ferreiras_p2_ap2/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import ucne.edu.victor_ferreiras_p2_ap2.ui.theme.Victor_Ferreiras_P2_AP2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
