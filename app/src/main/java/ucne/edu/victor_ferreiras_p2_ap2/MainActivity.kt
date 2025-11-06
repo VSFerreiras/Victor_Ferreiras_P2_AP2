1504cdd0ed27b52c8ba1ae878783165fa81c345b
victor <vsferreiras402@protonmail.com>
2025-11-05 21:41:46 -0400

    Agregado Screen al MainActivity.kt

--- app/src/main/java/ucne/edu/victor_ferreiras_p2_ap2/MainActivity.kt
+++ app/src/main/java/ucne/edu/victor_ferreiras_p2_ap2/MainActivity.kt
@@ -1,18 +1,11 @@
 package ucne.edu.victor_ferreiras_p2_ap2
 
 import android.os.Bundle
-
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
+import ucne.edu.victor_ferreiras_p2_ap2.presentation.GastoScreen
 import ucne.edu.victor_ferreiras_p2_ap2.ui.theme.Victor_Ferreiras_P2_AP2Theme
 
 @AndroidEntryPoint
@@ -22,29 +15,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Victor_Ferreiras_P2_AP2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GastoScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Victor_Ferreiras_P2_AP2Theme {
-        Greeting("Android")
     }
 }
