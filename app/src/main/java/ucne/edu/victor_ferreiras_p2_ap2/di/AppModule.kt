f3ef2cb312be726ecada0815a2c56d64e9283f01
victor <vsferreiras402@protonmail.com>
2025-11-05 21:31:25 -0400

    Agregado app service al appmodule

--- app/src/main/java/ucne/edu/victor_ferreiras_p2_ap2/di/AppModule.kt
+++ app/src/main/java/ucne/edu/victor_ferreiras_p2_ap2/di/AppModule.kt
@@ -1,6 +1,5 @@
 package ucne.edu.victor_ferreiras_p2_ap2.di
 
-import com.google.firebase.appdistribution.gradle.ApiService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
@@ -11,8 +10,8 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import ucne.edu.victor_ferreiras_p2_ap2.data.remote.ApiService
 import javax.inject.Singleton
-
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -43,4 +42,9 @@ object AppModule {
             .client(okHttpClient)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService =
+        retrofit.create(ApiService::class.java)
 }
